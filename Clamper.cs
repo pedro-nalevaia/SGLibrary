01a7b5b95c553e0f9643230f51812532e9049972
spring <pedrorafaelnalevaiaborges@gmail.com>
2026-08-13 22:17:33 -0300

    probably last version of the clamper

--- Clamper.cs
+++ Clamper.cs
@@ -3,39 +3,46 @@ using Godot;
 //the purpouses of this class is to be a reusable clamper you can't put on any object and use out of the box
 //you add this to an object putting the clamp data, and when you want to move it keeping the clamp you call 
 //its MoveWithClamp metod giving it a vector
-public partial class Clamper(){
-	public Vector2I ClampTopLeft;
-	public Vector2I ClampBottomRight;
+public partial class Clamper{
+	public Vector2 ClampTopLeft;
+	public Vector2 ClampBottomRight;
 	public Node2D Parent;
 
-	public void Clamper(Vector2I clamp_top_left, Vector2I clamp_bottom_right, Node2D parent)
+	public Clamper(Vector2 clamp_top_left, Vector2 clamp_bottom_right, Node2D parent)
 	{
 		ClampTopLeft = clamp_top_left;
 		ClampBottomRight = clamp_bottom_right;
 		Parent = parent;
 	}
 
-	public void MoveWithClamp(Vector2I Mov)
+	public void MoveWithClamp(Vector2 Mov)
 	{
-		Parent.Position += Mov;
+		//get position as a struct
+		//this needs to be done since modifying parts of the 
+		//Parent.position only modify a sturct copy inside this function
+		Vector2 pos = Parent.Position;
+		pos += Mov;
 
 		//CLAMPING
-		if (Parent.Position.X < ClampTopLeft.X)
+		if (pos.X < ClampTopLeft.X)
 		{
-			Parent.Position.X = ClampTopLeft.X;
+			pos.X = ClampTopLeft.X;
 		}
-		if (Parent.Position.X > ClampBottomRight.X)
+		if (pos.X > ClampBottomRight.X)
 		{
-			Parent.Position.X = ClampBottomRight.X;
+			pos.X = ClampBottomRight.X;
 		}
-		if (Parent.Positon.Y < ClampTopLeft.Y)
+		if (pos.Y < ClampTopLeft.Y)
 		{
-			Parent.Position.Y = ClampTopLeft.Y;
+			pos.Y = ClampTopLeft.Y;
 		}
-		if (Parent.Position.Y > ClampBottomRight.Y)
+		if (pos.Y > ClampBottomRight.Y)
 		{
-			Parent.Position.Y = ClampBottomRight.Y;
+			pos.Y = ClampBottomRight.Y;
 		}
+		//assign back
+		Parent.Position = pos;
 	}
 
 }
+
